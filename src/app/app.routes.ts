002d31204841356bd9d1299762695a885558474c
Garo Keshishian <garo.keshish@gmail.com>
2024-10-26 01:07:46 +0200

    Init Porject

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { WelcomeComponent } from './components/welcome/welcome.component'; // Import the WelcomeComponent
 
 export const routes: Routes = [
