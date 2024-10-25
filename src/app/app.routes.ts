f608c2773fefc1ab20ae75d722bc950a211ed3ab
Garo Keshishian <garo.keshish@gmail.com>
2024-10-26 01:03:33 +0200

    Init Porject

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { WelcomeComponent } from './components/welcome/welcome.component'; // Import the WelcomeComponent
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: WelcomeComponent, // Display WelcomeComponent at the root route
+  },
+  {
+    path: '',
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      // Add other child routes under MainLayoutComponent as needed
+    ],
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }, // Redirect unknown paths to the root
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
